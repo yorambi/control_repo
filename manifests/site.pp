90d0168893efa50fa4d6a247e40ee601ec276c5a
Yoram Samoray <yorambi@users.noreply.github.com>
2021-10-24 20:34:54 +0000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,19 +1,7 @@
 node default {
-  file { '/root/README':
-    ensure => file,
+  file {'/root/README':
+    ensure  => file,
     content => 'Hello World',
     owner   => 'root',
   }
-}
-
-node 'master.puppet.vm' {
-  include role::master_server
-}
-
-node /^web/ { 
-  include role::app_server
-
-}
-node /^db/ {
-  include role::db_server
 }
