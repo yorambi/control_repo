32e94fa6aa6fb740c0e824c316e9a33e59d5e414
Yoram Samoray <yorambi@users.noreply.github.com>
2021-10-26 09:25:48 +0000

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -2,7 +2,7 @@ class minecraft {
   file {'/opt/minecraft':
     ensure => directory,
   }
-  file {'/opt/minecraft/minecraft_server.1.12.2.jar':
+  file {'/opt/minecraft/minecraft_server.jar':
     ensure => file,
     source      => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
   }
