33e9c84d48f10931a9bdbee3f272d72842866653
Yoram Samoray <yorambi@users.noreply.github.com>
2021-10-26 04:58:46 +0000

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -4,10 +4,10 @@ class profile::agent_nodes {
   dockeragent::node {'db.puppet.vm':}
   host {'web.puppet.vm':
     ensure => present,
-    ip     => '172.18.0.2',
+    ip     => "${ipaddress}",
   }
   host {'db.puppet.vm':
     ensure => present,
-    ip     => '172.18.0.3',
+    ip     => "${ipaddress}",
   }
 }
